891f6d8012f60c0704ae6a29b571b0db1ddcce6b
victoria-nsl <vbnaish@yandex.ru>
2021-06-06 10:37:50 +0300

    Добавляет создание массива объявлений из 10 сгенерированных объектов

--- js/main.js
+++ js/main.js
@@ -14,3 +14,78 @@ const getRandomNonintegerNumber = function (minNumber, maxNumber, numberDecimals
 };
 
 getRandomNonintegerNumber(5.1545,10.25,2);
+
+//Функция перемешивания элементов в массиве, пример с сайта https://learn.javascript.ru/task/shuffle
+
+const mixArray = function(array) {
+  for (let counterFirst = array.length - 1; counterFirst > 0; counterFirst--) {
+    const counterSecond = Math.floor(Math.random() * (counterFirst + 1));
+    [array[counterFirst], array[counterSecond]] = [array[counterSecond], array[counterFirst]];
+  }
+  return array;
+};
+
+//Функция получения перемешанного массива случайной длины
+
+const getRandomLengthMixedArray = function(croppedArray) {
+  const randomNumberFirst = getRandomNumber(0,croppedArray.length-1);
+  const randomNumberSecond = getRandomNumber(0,croppedArray.length-1);
+  const newMixArray = mixArray(croppedArray);
+  return (randomNumberFirst > randomNumberSecond) ? newMixArray.slice(randomNumberSecond, randomNumberFirst) : newMixArray.slice(randomNumberFirst, randomNumberSecond);
+};
+
+//Функция по поиску случайного элемента в переданном массиве
+
+const getRandomArrayElement = function(elements) {
+  return elements[getRandomNumber(0, elements.length - 1)];
+};
+
+// Cоздание массива из 10 сгенерированных JS-объектов. Каждый объект массива — описание похожего объявления неподалёку.
+
+const TITLES = ['Лучшее предложение', 'Выгодное предложение', 'Уникальное жилье', 'Спецпредложение'];
+const HOUSING_TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const CHECKIN_TIMES = ['12:00', '13:00', '14:00'];
+const CHECKOUT_TIMES = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg','https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg','https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg]'];
+const DESCRIPTIONS = ['Помещение для большой компании', 'Помещение для семей с детьми', 'Помещение для двоих', 'Помещение для одиноких туристов'];
+
+const createOfferRentRealty = function(userNumber) {
+
+  const latitude = getRandomNonintegerNumber(35.65000,35.70000,5);
+  const longitude = getRandomNonintegerNumber(139.70000,139.80000,5);
+  const getAvatarValue = function() {
+    return userNumber < 10 ? `img/avatars/user${  0  }${userNumber}.png`: `img/avatars/user${userNumber}.png`;
+  };
+
+  return {
+    author: {
+      avatar: getAvatarValue(),
+    },
+
+    offer: {
+      title: getRandomArrayElement(TITLES),
+      address: `${latitude  }, ${  longitude}`,
+      price: getRandomNumber(100,1000),
+      type: getRandomArrayElement(HOUSING_TYPES),
+      rooms: getRandomNumber(1,4),
+      guests: getRandomNumber(1,10),
+      checkin: getRandomArrayElement(CHECKIN_TIMES),
+      checkout: getRandomArrayElement(CHECKOUT_TIMES),
+      features: getRandomLengthMixedArray(FEATURES),
+      description:getRandomArrayElement(DESCRIPTIONS),
+      photos: getRandomLengthMixedArray(PHOTOS),
+    },
+
+    location: {
+      lat: latitude,
+      lng: longitude,
+    },
+  };
+};
+
+let numberAvatars = new Array(10).fill(null).map((value,index) => value += index + 1);
+numberAvatars = mixArray(numberAvatars);
+
+new Array(10).fill(null).map((value, index) => createOfferRentRealty(numberAvatars[index]));
+
