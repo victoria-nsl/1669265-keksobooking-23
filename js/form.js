9bee1ae82d5e2fec6bde475aade369fb3e254c37
victoria-nsl <vbnaish@yandex.ru>
2021-06-22 11:33:32 +0300

    Добавляет валидацию полей "Заголовок объявления, цена за ночь, количество комнат и количество гостей"

--- js/form.js
+++ js/form.js
@@ -7,7 +7,7 @@ const formMap = document.querySelector('.map__filters');
 const listFeaturesMap = document.querySelector('.map__features');
 const filtersMap = document.querySelectorAll('.map__filter');
 
-//ФУНКЦИЯ ПЕРЕКЛЮЧИЕНИЯ АКТИВНОГО/НЕАКТИВНОГО СОСТОЯНИЯ
+//ФУНКЦИЯ ПЕРЕКЛЮЧЕНИЯ АКТИВНОГО/НЕАКТИВНОГО СОСТОЯНИЯ
 const  toggleInActiveStatePage  = (isInActive) => {
   if (isInActive) {
     formAd.classList.add('ad-form--disabled');
@@ -24,6 +24,58 @@ const  toggleInActiveStatePage  = (isInActive) => {
   filtersMap.forEach((filterMap) => {filterMap.disabled = isInActive;});
 };
 
-toggleInActiveStatePage(true);
+toggleInActiveStatePage(false);
+
+//ВАЛИДАЦИЯ ФОРМЫ
+
+//Заголовок объявления
+const titleAd = document.querySelector('#title');
+const MIN_TITLE_LENGTH = 30;
+const MAX_TITLE_LENGTH = 100;
+
+titleAd.addEventListener('input', () => {
+  const valueLength = titleAd.value.length;
+
+  if (valueLength < MIN_TITLE_LENGTH) {
+    titleAd.setCustomValidity(`Ещё ${  MIN_TITLE_LENGTH - valueLength } симв.`);
+  } else if (valueLength > MAX_TITLE_LENGTH) {
+    titleAd.setCustomValidity(`Удалите лишние ${  valueLength - MAX_TITLE_LENGTH } симв.`);
+  } else {
+    titleAd.setCustomValidity('');
+  }
+  titleAd.reportValidity();
+});
+
+//Цена за ночь
+const priceAd = document.querySelector('#price');
+const MAX_PRICE = 1000000;
+
+priceAd.addEventListener('input', () => {
+  if (priceAd.value > MAX_PRICE) {
+    priceAd.setCustomValidity('Цена не может быть больше 1 000 000');
+  } else {
+    priceAd.setCustomValidity('');
+  }
+  priceAd.reportValidity();
+});
+
+//Количество комнат и количество мест
+const roomNumber = document.querySelector('#room_number');
+const capacityRoom = document.querySelector('#capacity');
+const capacityRoomOptions = capacityRoom.querySelectorAll('option');
+const capacityRoomOptionNoGuests = capacityRoom.querySelector('[value="0"]');
+const ROOM_NO_GUESTS = 100;
 
 
+roomNumber.addEventListener('change', (evt) => {
+  const changeRoom = evt.target.value;
+  (capacityRoomOptions).forEach((capacityRoomOption) => {
+    if (changeRoom < ROOM_NO_GUESTS)  {
+      capacityRoomOption.disabled = capacityRoomOption.value > changeRoom;
+      capacityRoomOptionNoGuests.disabled = true;
+    } else {
+      capacityRoomOption.disabled = true;
+      capacityRoomOptionNoGuests.disabled = false;
+    }
+  });
+});
